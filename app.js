48426162b8c23f043a270f902926c184f983ae4d
Polykrovka <2595092@gmail.com>
2021-03-29 00:50:51 +0300

    refactor: add BEM notation according to rule 2.1 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ var createNewTaskElement=function(taskString){
     //Each elements, needs appending
     checkBox.type="checkbox";
     editInput.type="text";
-    editInput.className="all-inputs task";
+    editInput.className="list__input task";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
     editButton.className="list__button edit";
